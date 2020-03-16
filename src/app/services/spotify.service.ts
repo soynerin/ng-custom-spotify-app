2b25382dda863864e01dd47baf9ee4190b716322
Nerinni <neri.agustin.es@outlook.com>
2020-03-16 19:16:36 -0300

    Refactor de las peticiones get en los servicios

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,18 +8,22 @@ import { map } from 'rxjs/operators';
 })
 export class SpotifyService {
 
-  constructor( private http: HttpClient ) {
+  constructor( private http: HttpClient ) { }
 
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${ query }`;
+
+    const headers = new HttpHeaders({
+        Authorization: 'Bearer BQC0RGj--oCZXEkALbxqSLyLj_DgVDttrNq0DLNQbkfghGpxreZt7yrQAuq91Lwshc9zTgICRCGj_pQwrXA'
+    });
+
+    return this.http.get(url, { headers });
   }
 
   getNewReleases() {
 
-    const headers = new HttpHeaders({
-        Authorization: 'Bearer BQDakGlsK-BkCVkEMHDTy5foz5NpixVCrMidVbV-wUrHNmGiwLTrOVT-aSHoa5SgWWeuiYvjSmuvbvlPSLQ'
-    });
-
-    return this.http
-      .get('https://api.spotify.com/v1/browse/new-releases', { headers } )
+    return this
+      .getQuery('browse/new-releases')
       .pipe(
         // tslint:disable-next-line: no-string-literal
         map(data => data['albums'].items)
@@ -28,15 +32,11 @@ export class SpotifyService {
 
   getArtista( terminoBUsqueda: string ) {
 
-    const headers = new HttpHeaders({
-        Authorization: 'Bearer BQBN37Pd74v5g6mLvRtiDXTYVzDX6MlMjKmP4wRQTR75rXGsESpikaoMG_b4fQmvw6lAxSZMsu97lYQ9APM'
-    });
-
-    return this.http
-      .get(`https://api.spotify.com/v1/search?q=${terminoBUsqueda}&type=artist&limit=20`, { headers } )
+    return this
+      .getQuery(`search?q=${terminoBUsqueda}&type=artist&limit=20`)
       .pipe(
         // tslint:disable-next-line: no-string-literal
-        map(data => data['artists'].items )
+        map(data => data['artists'].items)
       );
   }
 }
